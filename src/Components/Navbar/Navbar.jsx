95bd77ae940e368045a0511996aaf28ac440bf39
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-06-10 15:11:11 -0700

    cartitems and total

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar=()=>{
 // setting a state for nav menu and conditionally applying the css on click of each header
 
     const [menu,setMenu]=useState("shop")
+    const {getTotalCartItems} =useContext(ShopContext)
 return(
     <div className="navbar">
         <div className="nav-logo">
@@ -24,7 +26,7 @@ return(
         <div className="nav-login-cart">
            <Link to='/login'><button>Login</button></Link> 
             <Link to='/cart'><img src={cart_icon} alt=''/></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
         
     </div>
