c59619fd4a319595bc80a6b0160dd50fff688903
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-06-10 16:49:19 -0700

    updated alt prop

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -17,7 +17,7 @@ const ProductDisplay=(props)=>{
                     <img src={product.image} alt=''/>
                 </div>
                 <div className="productdisplay-img">
-                    <img className="productdisplay-main-img" src={product.image}/>
+                    <img className="productdisplay-main-img" src={product.image} alt=""/>
                 </div>
             </div>
             <div className="productdisplay-right">
